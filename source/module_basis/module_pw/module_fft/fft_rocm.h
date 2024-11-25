4e12e56de0a2dcb21cb22a37a76b908184e44019
A-006 <3158793232@qq.com>
2024-11-25 10:28:12 +0800

    update the rcom to rocm

--- source/module_basis/module_pw/module_fft/fft_rocm.h
+++ source/module_basis/module_pw/module_fft/fft_rocm.h
@@ -6,11 +6,11 @@
 namespace ModulePW
 {
 template <typename FPTYPE>
-class FFT_RCOM : public FFT_BASE<FPTYPE>
+class FFT_ROCM : public FFT_BASE<FPTYPE>
 {
     public:
-        FFT_RCOM(){};
-        ~FFT_RCOM(){}; 
+        FFT_ROCM(){};
+        ~FFT_ROCM(){}; 
         
         void setupFFT() override; 
 
@@ -57,9 +57,5 @@ class FFT_RCOM : public FFT_BASE<FPTYPE>
         mutable std::complex<double>* z_auxr_3d = nullptr; // fft space
 
 };
-template FFT_RCOM<float>::FFT_RCOM();
-template FFT_RCOM<float>::~FFT_RCOM();
-template FFT_RCOM<double>::FFT_RCOM();
-template FFT_RCOM<double>::~FFT_RCOM();
 }// namespace ModulePW
 #endif
