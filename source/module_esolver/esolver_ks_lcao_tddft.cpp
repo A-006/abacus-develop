6c346f3d914c613da71aedf88e414ae0f4212194
A-006 <3158793232@qq.com>
2024-12-03 10:44:58 +0800

    change ucell in module_io/td_current_io.cpp

--- source/module_esolver/esolver_ks_lcao_tddft.cpp
+++ source/module_esolver/esolver_ks_lcao_tddft.cpp
@@ -357,7 +357,8 @@ void ESolver_KS_LCAO_TDDFT::after_scf(UnitCell& ucell, const int istep)
         elecstate::DensityMatrix<std::complex<double>, double>* tmp_DM
             = dynamic_cast<elecstate::ElecStateLCAO<std::complex<double>>*>(this->pelec)->get_DM();
 
-        ModuleIO::write_current(istep,
+        ModuleIO::write_current(ucell,
+                                istep,
                                 this->psi,
                                 pelec,
                                 kv,
