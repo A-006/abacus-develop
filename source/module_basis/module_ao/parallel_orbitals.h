b5c3ac218a754d8c9f39855f5371f98a5115122b
liiutao <3158793232@qq.com>
2024-12-26 16:38:30 +0800

    update nnr init data

--- source/module_basis/module_ao/parallel_orbitals.h
+++ source/module_basis/module_ao/parallel_orbitals.h
@@ -29,7 +29,7 @@ public:
     /// number of elements(basis-pairs) in this processon
     /// on all adjacent atoms-pairs(2D division)
     ///---------------------------------------
-    int nnr=0;
+    int nnr=1;
 	int *nlocdim;
 	int *nlocstart;
     
